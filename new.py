ec7a5a0105bc399af7130334ce8faebaf63b05c5
Rishabh Jain <rishabh2408@gmail.com>
2018-07-06 07:50:33 +0530

    complete

---
+++ new.py
@@ -0,0 +1,22 @@
+import json
+from difflib import get_close_matches
+data=json.load(open("data.json"))
+def meaning(w):
+	if w in data:
+		return data[w.lower()]
+	elif len(get_close_matches(w,data.keys()))>0:
+		ch =input("Did you mean %s instead: " %get_close_matches(w,data.keys())[0])
+		if ch.lower()=='yes' or ch.lower()=='y':
+			return meaning(get_close_matches(w,data.keys())[0])
+		else:
+			return "The word doesnt exist"
+	else:
+		return "The word doesnt exist"
+
+
+while True:
+	word=input("Enter Word: ")
+	print(meaning(word))
+	ch =input("Want to continue? ")
+	if ch.lower()=='no' or ch.lower()=='n':
+		break
