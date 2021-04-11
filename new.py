4406e48ef2fea663a6f999d9a47842c62b7df68a
rishabh2408 <rishabh2408@gmail.com>
2021-04-11 21:45:15 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,7 +1,7 @@
 import json
 from difflib import get_close_matches
 data=json.load(open("data.json"))
-def meaning(w):
+def meaning(w): 
 	if w in data:
 		return data[w.lower()]
 	elif len(get_close_matches(w,data.keys()))>0:
