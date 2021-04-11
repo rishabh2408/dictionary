ccea62ca6c6a13409cb9512e15e8815478f20a47
rishabh2408 <rishabh2408@gmail.com>
2021-04-11 21:34:31 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,7 +2,7 @@ import json
 from difflib import get_close_matches
 data=json.load(open("data.json"))
 def meaning(w):
-	if w in data:
+	if w in data: 
 		return data[w.lower()]
 	elif len(get_close_matches(w,data.keys()))>0:
 		ch =input("Did you mean %s instead: " %get_close_matches(w,data.keys())[0])
